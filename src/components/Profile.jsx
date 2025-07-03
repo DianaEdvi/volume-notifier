578493d33108a0a5b790c2059f69150f4d057d33
Diana <edvi.diana@gmail.com>
2025-07-03 23:50:21 +0900

    I did a bunch of work. I added a black background woo

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,15 @@
+import "../index.css";
+
 export default function Profile() {
     return (
-        <img
-            src="https://i.imgur.com/MK3eW3Am.jpg"
-            alt="Katherine Johnson"
-        />
+        <div>
+
+            <img
+                src="https://i.imgur.com/MK3eW3Am.jpg"
+                alt="Katherine Johnson"
+            />
+            <h1>Hello</h1>
+        </div>
+
     )
 }
