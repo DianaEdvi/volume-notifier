25cc7ca7ce09306a7f18b78683d84659c7e3c599
Diana <edvi.diana@gmail.com>
2025-07-08 18:16:55 +0900

    Modified oscilloscope function to get more precise values and calculate decibel strength

--- src/components/audioTracker.jsx
+++ src/components/audioTracker.jsx
@@ -15,29 +15,34 @@ function MicOscilloscope(){
         let dataArray;
         let canvas;
         let canvasCtx;
+        let WIDTH = 600, HEIGHT = 400;
 
         async function setup(){
 
             try {
-                const audioCtx = new AudioContext(); // Create AudioContext object (required for Web Audio API)
-
+                // Create AudioContext object (required for Web Audio API)
                 // Request permission to use mic (and wait)
+                // Create a stream to mic and set it to source
+                const audioCtx = new AudioContext();
                 const stream = await navigator.mediaDevices.getUserMedia({audio: true});
-
                 const source = audioCtx.createMediaStreamSource(stream);
-                analyser = audioCtx.createAnalyser(); // Create AudioNode object that tracks audio data
-                analyser.fftSize = 2048; // Fast Fourier Transform size (aka how many samples to take of audio per frame)
 
-                bufferLength = analyser.frequencyBinCount; // Half the size of fftSize bcs the waves are mirrored horizontally
-                dataArray = new Uint8Array(bufferLength); // Create array of bytes (0-255) of length bufferLength
-                analyser.getByteTimeDomainData(dataArray); // Copy the audio wave values into the array
+                // Create AudioNode object that tracks audio data
+                // Set Fast Fourier Transform size (aka how many samples to take of audio per frame)
+                // Halve the size of fftSize bcs the waves are mirrored horizontally
+                // Create array of floats of length bufferLength
+                analyser = audioCtx.createAnalyser();
+                analyser.fftSize = 2048;
+                bufferLength = analyser.fftSize;
+                dataArray = new Float32Array(bufferLength);
 
-                // Connect the source to be analyzed
+                // Connect the source (mic) to be analyzed
                 source.connect(analyser);
 
                 // Get a canvas defined with ID "oscilloscope"
-                canvas = document.getElementById("oscilloscope");
                 // Get drawing API for 2d canvas
+                // Call draw function
+                canvas = document.getElementById("oscilloscope");
                 canvasCtx = canvas.getContext("2d");
                 draw();
 
@@ -49,32 +54,33 @@ function MicOscilloscope(){
 
         // Draw an oscilloscope of the current audio source
         function draw() {
-            requestAnimationFrame(draw);
+            let drawVisual = requestAnimationFrame(draw);
 
-            analyser.getByteTimeDomainData(dataArray);
+            // Fill dataArray with the current audio waveform samples as floating-point values between -1.0 and 1.0
+            analyser.getFloatTimeDomainData(dataArray);
 
-            // I think I will move this styling into somewhere else? Not sure yet.
+            // I still think I am going to move the draw stuff else where?
+            new RootMeanSquareToDecibel(dataArray);
+
+            //Draw the canvas and the waveform
             canvasCtx.fillStyle = "rgb(200 200 200)";
-            canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
-
+            canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
             canvasCtx.lineWidth = 2;
             canvasCtx.strokeStyle = "rgb(0 0 0)";
-
             canvasCtx.beginPath();
 
-            const sliceWidth = (canvas.width * 1.0) / bufferLength;
+            const sliceWidth = (WIDTH) / bufferLength;
             let x = 0;
 
             for (let i = 0; i < bufferLength; i++) {
-                const v = dataArray[i] / 128.0;
-                const y = (v * canvas.height) / 2;
+                const v = dataArray[i] * 200.0;
+                const y = HEIGHT / 2 + v;
 
                 if (i === 0) {
                     canvasCtx.moveTo(x, y);
                 } else {
                     canvasCtx.lineTo(x, y);
                 }
-
                 x += sliceWidth;
             }
 
@@ -87,6 +93,26 @@ function MicOscilloscope(){
     }, [])
 
     return <canvas id="oscilloscope" width={500} height={500}></canvas>
+}
+
+/**
+ * Calculate RMS (Root Mean Square) of the samples in dataArray and convert it to decibel values
+ * @param dataArray
+ * @constructor
+ */
+function RootMeanSquareToDecibel(dataArray){
+    // I also think this stuff should go elsewhere but I'm not sure where yet?
+
+    let sumOfSquares = 0;
+
+    for (let i = 0; i < dataArray.length; i++){
+        sumOfSquares += dataArray[i] * dataArray[i];
+    }
+    let rms = Math.sqrt(sumOfSquares/dataArray.length);
+
+    let dB = 20 * Math.log10(rms);
+    console.log(dB);
+
 }
 
 export default MicOscilloscope;
